1d9edbc508f2cc59e1b1fb1b7950efba5ac05021
Swan <contact@swan.moe>
2017-06-23 14:49:25 -0400

    Change: User test descriptions

--- tests/users.test.js
+++ tests/users.test.js
@@ -5,7 +5,7 @@ describe('users', () => {
 
     // Test for users.getUserById(id)
     describe('#getUserById', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should get the information for a given user by their user id from Ripple\'s API', (done) => {
             users.getUserById(1298)
                 .then((user) => {
                     //console.log(user);
@@ -19,7 +19,7 @@ describe('users', () => {
 
     // Test for users.getUserByName(username)
     describe('#getUserByName', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should get the information for a given user by their username from Ripple\'s API', (done) => {
             users.getUserByName('Nyo')
                 .then((user) => {
                     //console.log(user);
@@ -33,7 +33,7 @@ describe('users', () => {
 
     // Test for users.getFullUserById(id)
     describe('#getFullUserById', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should get all possible information for a given user by their user id from Ripple\'s API', (done) => {
             users.getFullUserById(1298)
                 .then((user) => {
                     //console.log(user);
@@ -47,7 +47,7 @@ describe('users', () => {
 
     // Test for users.getFullUserByName(username)
     describe('#getFullUserByName', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should get all possible information for a given user by their username from Ripple\'s API', (done) => {
             users.getFullUserByName('Shaural')
                 .then((user) => {
                     //console.log(user);
@@ -61,7 +61,7 @@ describe('users', () => {
 
      // Test for users.getUserByNameAka(nameAka)
     describe('#getUserByNameAka', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should get the information for a given user by their name_aka from Ripple\'s API', (done) => {
             users.getUserByNameAka('Square')
                 .then((user) => {
                     //console.log(user);
@@ -75,7 +75,7 @@ describe('users', () => {
 
      // Test for users.getUsersByCountry(country)
     describe('#getUsersByCountry', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users from a given country from Ripple\'s API', (done) => {
             users.getUsersByCountry('KP')
                 .then((users) => {
                     //console.log(users);
@@ -89,7 +89,7 @@ describe('users', () => {
 
      // Test for users.getUsersByPrivilegeGroup(privilegeGroup)
     describe('#getUsersByPrivilegeGroup', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users from a given privilege group from Ripple\'s API', (done) => {
             users.getUsersByPrivilegeGroup('Community Manager')
                 .then((users) => {
                     //console.log(users);
@@ -103,7 +103,7 @@ describe('users', () => {
 
      // Test for users.getUsersById(userIds)
     describe('#getUsersById', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users by their user ids from Ripple\'s API', (done) => {
             users.getUsersById([1298, 1000, 1006])
                 .then((users) => {
                     //console.log(users);
@@ -117,7 +117,7 @@ describe('users', () => {
 
      // Test for users.getUsersByName(usernames)
     describe('#getUsersByName', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users by their usernames from Ripple\'s API', (done) => {
             users.getUsersByName(['Nyo', 'Howl', 'Shaural', 'Swan', 'AiAe'])
                 .then((users) => {
                     //console.log(users);
@@ -131,7 +131,7 @@ describe('users', () => {
 
      // Test for users.getUsersByNameAka(nameAkas)
     describe('#getUsersByNameAka', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users by their name_aka from Ripple\'s API', (done) => {
             users.getUsersByNameAka(['pescado', 'square'])
                 .then((users) => {
                     //console.log(users);
@@ -145,7 +145,7 @@ describe('users', () => {
 
      // Test for users.getUsersByCountries(countries)
     describe('#getUsersByCountries', () => { 
-        it('should get the information for a given user from Ripple\'s API', (done) => {
+        it('should return a list of users by their countries from Ripple\'s API', (done) => {
             users.getUsersByCountries(['KP', 'IL'])
                 .then((users) => {
                     //console.log(users);
