d44526fe2bdda4c5227cdc7ef11ade81c4ea0800
Swan <contact@swan.moe>
2017-06-22 21:20:28 -0400

    Add: GET entire /users section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cheesegull = require('./lib/cheesegull');
 const scores = require('./lib/scores');
+const users = require('./lib/users');
 
 module.exports = {
     getBeatmapInfo: cheesegull.getBeatmapInfo,
@@ -7,5 +8,14 @@ module.exports = {
     searchBeatmaps: cheesegull.searchBeatmaps,
     getBeatmapScores: scores.getBeatmapScores,
     getUserBest: scores.getUserBest,
-    getUserRecent: scores.getUserRecent
+    getUserRecent: scores.getUserRecent,
+    getUserById: users.getUserById,
+    getUserByName: users.getUserByName,
+    getUsersByNameAka: users.getUserByNameAka,
+    getUsersByCountry: users.getUsersByCountry,
+    getUsersByPrivilegeGroup: users.getUsersByPrivilegeGroup,
+    getUsersById: users.getUsersById,
+    getUsersByName: users.getUsersByName,
+    getUsersByNameAka: users.getUsersByNameAka,
+    getUsersByCountry: users.getUsersByCountries
 };
---
+++ tests/users.test.js
@@ -0,0 +1,132 @@
+const expect = require('expect');
+const users = require('../lib/users');
+
+describe('users', () => {
+
+    // Test for users.getUserById(id)
+    describe('#getUserById', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUserById(1298)
+                .then((user) => {
+                    //console.log(user);
+                    done();
+                    expect(user)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+    // Test for users.getUserByName(username)
+    describe('#getUserByName', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUserByName('Nyo')
+                .then((user) => {
+                    //console.log(user);
+                    done();
+                    expect(user)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUserByNameAka(nameAka)
+    describe('#getUserByNameAka', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUserByNameAka('Square')
+                .then((user) => {
+                    //console.log(user);
+                    done();
+                    expect(user)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersByCountry(country)
+    describe('#getUsersByCountry', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersByCountry('KP')
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersByPrivilegeGroup(privilegeGroup)
+    describe('#getUsersByPrivilegeGroup', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersByPrivilegeGroup('Community Manager')
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersById(userIds)
+    describe('#getUsersById', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersById([1298, 1000, 1006])
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersByName(usernames)
+    describe('#getUsersByName', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersByName(['Nyo', 'Howl', 'Shaural', 'Swan', 'AiAe'])
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersByNameAka(nameAkas)
+    describe('#getUsersByNameAka', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersByNameAka(['pescado', 'square'])
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+     // Test for users.getUsersByCountries(countries)
+    describe('#getUsersByCountries', () => { 
+        it('should get the information for a given user from Ripple\'s API', (done) => {
+            users.getUsersByCountries(['KP', 'IL'])
+                .then((users) => {
+                    //console.log(users);
+                    done();
+                    expect(users)
+                        .toExist()
+                        .toBeA('object')                        
+                })
+        });
+    });
+
+});
